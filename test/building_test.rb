642d8b5c2caad87569ee6c6ea2b7529ef8f3bdf8
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-14 10:13:31 -0600

    Create test to display annual breakdown

--- test/building_test.rb
+++ test/building_test.rb
@@ -120,6 +120,21 @@ class BuildingTest < Minitest::Test
     assert_equal building.units_by_number_of_bedrooms, {4=>"B2", 2=>"B2", 1=>"A1"}
   end
 
+  def test_it_can_display_annual_breakdown
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    unit3 = Apartment.new({number: "B2", monthly_rent: 2000, bathrooms: 2, bedrooms: 4})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    building.add_unit(unit3)
+
+
+    assert_equal building.annual_breakdown, 2
+  end
+
+
+
 
 
 end
