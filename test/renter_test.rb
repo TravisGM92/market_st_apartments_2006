d74d835cd14b2d7b893fe1920a57606d045bfbcd
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-14 09:07:54 -0600

    Create test for testing it has a name

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -5,9 +5,16 @@ require './lib/renter'
 class RenterTest < Minitest::Test
 
   def test_it_exists
-      renter1 = Renter.new
+      renter1 = Renter.new("Jim")
 
       assert_instance_of Renter, renter1
+  end
+
+  def test_it_has_a_name
+    renter1 = Renter.new("Jessie")
+
+
+    assert_equal "Jessie", renter1.name
   end
 
 end
